53d56b1945ff2bafc6cd761fe446fa5f4011e728
Rosy Arr <rmarreola@miners.utep.edu>
2020-02-09 15:41:34 -0700

    color event listeners pt2

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,8 +21,23 @@ for(var i=0; i<squares.length; i++){
   // Add click listeners to squares
   squares[i].addEventListener("click", function(){
     // Grab color of clicked square
-    alert(this.style.backgroundColor);
+    var clickedColor = this.style.backgroundColor;
     // Compare color of pickedSquare
-    
+    if (clickedColor === pickedColor){
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+    }
+    else{
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
   });
+}
+
+function changeColors(color) {
+  // Loop through all squares
+  for(var i=0; i<squares.length; i++){
+    // Change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
 }
